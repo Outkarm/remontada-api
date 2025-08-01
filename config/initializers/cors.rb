3084b302339ec2c49c0d0b3a031231a4da2f4f82
Outkarm <jkpordje@gmail.com>
2025-08-01 13:24:37 +0000

    allw rack cors for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'  # 👈 or '*' for testing only
+    origins 'http://localhost:5173', 'https://remontada-baej.onrender.com'
 
     resource '*',
       headers: :any,
